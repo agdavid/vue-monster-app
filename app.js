a12fcd5d2d98bc3ff41b5a6561bf14e981555bd5
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2020-10-27 15:45:00 -0400

    implement and limit special attack

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0,
         };
     },
     computed: {
@@ -15,10 +16,15 @@ const app = Vue.createApp({
         },
         playerBarStyles() {
             return {width: this.playerHealth + '%'}
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0
         }
     },
     methods: {
         attackMonster() {
+            // increment round
+            this.currentRound++;
             // random between 5 and 12
             const attackValue = getRandomValue(5,12);
             this.monsterHealth -= attackValue;
@@ -29,8 +35,14 @@ const app = Vue.createApp({
             // random between 5 and 12
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
-    }
+    },
 });
 
 app.mount('#game');
