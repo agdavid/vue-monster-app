9b3b6d2a762597749f0a7c3f06697211e6c5b753
Ajay David <ajaydavid@ANTONIO-ALGOLIA-MBP.local>
2020-10-27 15:29:32 -0400

    create vue data and methods for attacks

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const app = Vue.createApp({
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max-min)) + min;
+}
 
+const app = Vue.createApp({
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+        };
+    },
+    methods: {
+        attackMonster() {
+            // random between 5 and 12
+            const attackValue = getRandomValue(5,12);
+            this.monsterHealth -= attackValue;
+            // trigger monster's attack on player
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            // random between 5 and 12
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+        }
+    }
 });
 
 app.mount('#game');
